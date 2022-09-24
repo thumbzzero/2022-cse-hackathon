2142341e10ff7b21e2801573aa7ca675617f1fde
oyoungsun <plusmate@naver.com>
2022-09-24 22:06:24 +0900

    reflog

--- backend/src/main/java/com/example/banktreeserver/entity/Banktree.java
+++ backend/src/main/java/com/example/banktreeserver/entity/Banktree.java
@@ -16,22 +16,21 @@ public class Banktree {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String startName;
+
     private float startX;
     private float startY;
-    private String goalName;
+
     private float goalX;
     private float goalY;
-    private float density;
 
-    @Builder
-    public Banktree(Long id){
-        this.id = id;
-        this.startName="testcase1";
-        this.goalName="testcase1";
-        this.startX= (float) 1234.56;
-        this.startY= (float) 1234.56;
-        this.goalX= (float) 1234.566;
-        this.goalY= (float) 1234.566;
-    }
+//    @Builder
+//    public Banktree(Long id){
+//        this.id = id;
+//        this.startName="testcase1";
+//        this.goalName="testcase1";
+//        this.startX= (float) 1234.56;
+//        this.startY= (float) 1234.56;
+//        this.goalX= (float) 1234.566;
+//        this.goalY= (float) 1234.566;
+//    }
 }
